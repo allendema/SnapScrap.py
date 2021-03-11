bfb1b30ace31d073ee6fbdd37fdb199cb7e04b35
Allen <64094914+allendema@users.noreply.github.com>
2021-03-11 23:31:37 +0100

    Use headers

--- SnapScrap.py
+++ SnapScrap.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 
 from bs4 import BeautifulSoup
-from time import sleep
 import requests
 import json
 import os
 
-query = input("Enter a username: ")
+query = input("Enter a username: \nUsername bitte eintippen: ")
 
 path = query
 try:
-	os.mkdir(path)
+    os.mkdir(path)
 except FileExistsError:
-	print("This user has a folder in your System.")
+    print("This user has a folder in your System.")
 
 os.chdir(path)
 
@@ -21,14 +20,18 @@ url = "https://story.snapchat.com/u/"
 mix = url + str(query)
 print(mix)
 
-r = requests.get(mix)
+headers = {
+    'User-Agent': '"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0',
+}
+
+r = requests.get(mix, headers = headers)
 
 if r.ok:
-	print("\033[1;32;40m Snapchat site is Responding :)  \n")
-	
+    print("\033[1;32;40m Snapchat site is Responding :)  \n")
+    
 else:
-	print("\033[31m Snap! No connection with Snap!")
-	
+    print("\033[31m Snap! No connection with Snap!")
+    
 soup = BeautifulSoup(r.content, "html.parser")
 
 snaps = soup.find_all("script")[4].string.strip()
@@ -38,26 +41,28 @@ print("The script exists. \n")
 data = json.loads(snaps)
 
 #displayUsername = data["props"]["userDisplayInfo"]["username"]
-#print("You have searched for:", displayUsername, "\n")
+#print("You have searched for", displayUsername, \n")
 
 try:
-	for i in data["props"]["story"]["snapList"]:
-		file_url = (i["snapUrls"]["mediaUrl"])
-		file_name = file_url.split('/')[-3][:7]
-		
-		print(file_url)
-		
-		r = requests.get(file_url, stream=True)
+    for i in data["props"]["story"]["snapList"]:
 
-		if r.status_code == 200:
-			with open(file_name, 'wb') as f:
-				for chunk in r:
-					f.write(chunk)
-				print(f"\033[33mGetting posts of:\033[33m {query}")
-		else:
-			print("Cannot make connection to download media.")
-		
+        file_url = (i["snapUrls"]["mediaUrl"])
+        file_name = file_url.split('/')[-3][:7]
+        
+        print(file_url)
+        
+        r = requests.get(file_url, stream=True, headers = headers)
+
+        if r.status_code == 200:
+            with open(file_name, 'wb') as f:
+                for chunk in r:
+                    f.write(chunk)
+                print(f"\033[33mGetting posts of:\033[33m {query}")
+        else:
+            print("Cannot make connection to download media!")
+        
 except KeyError:
-	print("This user has no Story in the last 24h.")
+    print("This user has no Story in the last 24h.")
 else:
-	print("\nAt least one Story found. Successfully Downloaded.")
+    print("\nAt least one Story found. Successfully Downloaded.")
+
