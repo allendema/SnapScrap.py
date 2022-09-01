1eb198db110be1ba27150d4f65878e3b4679cc8a
Allen <64094914+allendema@users.noreply.github.com>
2022-09-01 18:44:47 +0200

    [enh] Use functions and pycodestyle
    Updat UA Header
    Print the time script took
    Use pycodestyle

--- SnapScrap.py
+++ SnapScrap.py
@@ -1,104 +1,154 @@
 #!/usr/bin/python3
+__author__ = "https://codeberg.org/allendema"
+__copyright__ = "Allen Dema 2020-2022"
+__license__ = "https://github.com/allendema/SnapScrap.py/blob/main/LICENSE"
 
-from bs4 import BeautifulSoup
 from time import sleep
-import requests
+import time
 import json
 import sys
 import os
 
-query = input("Enter a username: ")
-path = query
+from bs4 import BeautifulSoup
+import requests
 
-try:
-	os.mkdir(path)
-except FileExistsError:
-	print("This user has a folder in your System.")
 
-os.chdir(path)
+def user_input():
+	"""Check for username argument
+	Otherwise get it from user input"""
 
-url = "https://story.snapchat.com/@"
+	try:
+		username = sys.argv[1]
+	except Exception:
+		username = input("Enter a username: ")
 
-mix = url + str(query)
+	path = username
+
+	if os.path.exists(path):
+		print("This user has a folder in your System.")
+
+	else:
+		os.mkdir(path)
+
+	os.chdir(path)
+
+	return username
+
+
+YELLOW = "\033[1;32;40m"
+RED = "\033[31m"
+
+base_url = "https://story.snapchat.com/@"
+username = user_input()
+
+mix = base_url + username
 print(mix)
 
-headers = {
-	'User-Agent': '"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0',
-}
 
-r = requests.get(mix, headers=headers)
+def get_json():
+	headers = {
+		'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/103.0.2',
+	}
 
-if r.ok:
-	print("\033[1;32;40m Snapchat site is Responding :)  \n")
+	r = requests.get(mix, headers=headers)
 
-else:
-	print("\033[31m Snap! No connection with Snap!")
-	sys.exit(1)
+	if r.ok:
+		#print(f"{YELLOW} Snapchat site is Responding :)  \n")
+		pass
 
-soup = BeautifulSoup(r.content, "html.parser")
-#print(soup)
+	else:
+		sys.exit(f"{RED} Oh Snap! No connection with Snap!")
 
-snaps = soup.find(id="__NEXT_DATA__").string.strip()
+	soup = BeautifulSoup(r.content, "html.parser")
 
+	# Find the script with the JSON data on the site
+	snaps = soup.find(id="__NEXT_DATA__").string.strip()
 
-print("The script exists. \n")
+	#print("The script exists. \n")
+	data = json.loads(snaps)
 
-data = json.loads(snaps)
-
-try:
-	bitmoji = data["props"]["pageProps"]["userProfile"]["publicProfileInfo"]["snapcodeImageUrl"]
-	bio = data["props"]["pageProps"]["userProfile"]["publicProfileInfo"]["bio"]
-
-except KeyError:
-	bitmoji = data["props"]["pageProps"]["userProfile"]["userInfo"]["snapcodeImageUrl"]
-	bio = data["props"]["pageProps"]["userProfile"]["userInfo"]["displayName"]
-	print("Here is the Bio:\n", bio, "\nand Bitmoji:\n", bitmoji)
-	print("\nThis user is private.")
-	sys.exit(1)
-
-print("Bio of the user:\n", bio)
-print("\nHere is the Bitmoji:\n", bitmoji)
+	return data
 
 
-print(f"\n\033[33mGetting posts of:\033[33m {query}\n")
+def profile_metadata(json_dict=get_json()):
+	"""Detect public profile, then print bio and bitmoji"""
+	# if public
+	try:
+		bitmoji = json_dict["props"]["pageProps"]["userProfile"]["publicProfileInfo"]["snapcodeImageUrl"]
+		bio = json_dict["props"]["pageProps"]["userProfile"]["publicProfileInfo"]["bio"]
 
-try:
-	# Find all links with a for-loop
-	for i in data["props"]["pageProps"]["story"]["snapList"]:
+	# if not public
+	except KeyError:
+		bitmoji = json_dict["props"]["pageProps"]["userProfile"]["userInfo"]["snapcodeImageUrl"]
+		bio = json_dict["props"]["pageProps"]["userProfile"]["userInfo"]["displayName"]
 
-		file_url = i["snapUrls"]["mediaUrl"]
+		print(f"{YELLOW}Here is the Bio: \n {bio}\n")
+		print(f"Bitmoji:\n {bitmoji}\n")
+		print(f"{RED} This user is private.")
 
-		if file_url == "":
-			print("There is a Story but no URL is provided by Snapchat.")
-			continue
+		sys.exit(1)
 
-		# Download media and use a short unique file_name
-		r = requests.get(file_url, stream=True, headers=headers)
+	print(f"{YELLOW}\nBio of the user:\n", bio)
+	print(f"\nHere is the Bitmoji:\n {bitmoji} \n")
 
-		if "image" in r.headers['Content-Type']:
-			file_name = r.headers['ETag'].replace('"', '') + ".jpeg"
-			print(file_name)			
+	print(f"Getting posts of: {username}\n")
 
-		elif "video" in r.headers['Content-Type']:
-			file_name = r.headers['ETag'].replace('"', '') + ".mp4"
-			print(file_name)
 
-		#  Check if this file / file_name exists
-		if os.path.isfile(file_name) :
-			continue
-	
-		#  Sleep a bit
-		sleep(0.3)	
+def download_media(json_dict=get_json()):
+	"""Print media URLs and download media."""
+	try:
+		# Get all links with a for-loop
+		for i in json_dict["props"]["pageProps"]["story"]["snapList"]:
 
-		if r.status_code == 200:
-			with open(file_name, 'wb') as f:
-				for chunk in r:
-					f.write(chunk)
+			file_url = i["snapUrls"]["mediaUrl"]
 
-		else:
-			print("Cannot make connection to download media!")
+			if file_url == "":
+				print("There is a Story but no URL is provided by Snapchat.")
+				continue
 
-except KeyError:
-	print("No user stories found for the last 24h.")
-else:
-	print("\nAt least one Story found. Successfully Downloaded.")
+			# Download media and use a short unique file_name
+			r = requests.get(file_url, stream=True, headers=headers)
+			#print(r.headers['ETag'])
+			if "image" in r.headers['Content-Type']:
+				file_name = r.headers['ETag'].replace('"', '') + ".jpeg"
+				#print(file_name)
+
+			elif "video" in r.headers['Content-Type']:
+				file_name = r.headers['ETag'].replace('"', '') + ".mp4"
+				#print(file_name)
+
+			#  Check if this file / file_name exists
+			if os.path.isfile(file_name):
+				continue
+
+			#  Sleep a bit
+			sleep(0.3)
+
+			if r.status_code == 200:
+				with open(file_name, 'wb') as f:
+					for chunk in r:
+						f.write(chunk)
+
+			else:
+				print("Cannot make connection to download media!")
+
+	except KeyError:
+		print(f"{RED}No user stories found for the last 24h.")
+	else:
+		print("\nAt least one Story found. Successfully Downloaded.")
+
+
+def main():
+	start = time.perf_counter()
+
+	profile_metadata()
+	download_media()
+
+	end = time.perf_counter()
+	total = end - start
+
+	print(f"\n\nTotal time: {total}")
+
+
+if __name__ == "__main__":
+	main()
